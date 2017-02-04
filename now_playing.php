8c9b2733b8f616f82d4a5f9b73a6fe71f04cbc09
Sulochana Kodituwakku <sulochana.456@live.com>
2017-02-04 22:41:10 +0530

    now playing floarbar

---
+++ now_playing.php
@@ -0,0 +1,25 @@
+<div class="now_playing_bar">
+    <div class="container">
+        <div class="row nowplaying_status">
+            <span class="glyphicon glyphicon-play" aria-hidden="true"></span> Now Playing : Bad Boyz (Feat. Bobby Biscayne, Austin Mahone & Pitbull)
+        </div>
+        <div class="row nowplaying_action_row" >
+            <div class="col-md-5">
+                <div class="btn-group" role="group" aria-label="Make your vote">
+                    <button type="button" class="btn btn btn-success nowplaying_buttons"> <span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Up Vote</button>
+                    <button type="button" class="btn btn btn-danger nowplaying_buttons"> <span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Down Vote</button>
+                    <button type="button" class="btn btn btn-default nowplaying_buttons"> <span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span> Skip This Song</button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row skip_vote_txt">Need 7 more votes to skip this song</div>
+                <div class="row">
+                    <div class="progressbar_main">
+                        <div class="progressbar_value"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+</div>
