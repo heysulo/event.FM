c9df9388c5c6472fc762d6cf98e9d897ca2ccf16
Sulochana Kodituwakku <sulochana.456@live.com>
2017-02-08 01:21:12 +0530

    going azure

--- path.php
+++ path.php
@@ -1,7 +1,7 @@
 <?php
 defined("event.fm_optimus") or die("CSI_PATH");
-$publicPath = "http://".$_SERVER['HTTP_HOST']."/public/";
-$host = "http://".$_SERVER['HTTP_HOST'];
+$publicPath = "https://".$_SERVER['HTTP_HOST']."/public/";
+$host = "https://".$_SERVER['HTTP_HOST']."/";
 
 if ($_SERVER['HTTP_HOST'] == 'localhost'){
     $publicPath = "http://".$_SERVER['HTTP_HOST']."/event.FM/public/";
